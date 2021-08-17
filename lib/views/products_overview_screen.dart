7ec697925aa337ddf70909a2352200d1f3830034
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-08-17 16:02:59 -0300

    Update products_overview_screen.dart

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,11 +1,7 @@
+import 'package:eshop/widgets/product_grid.dart';
 import 'package:flutter/material.dart';
-import '../data/dummy_data.dart';
-import '../models/product.dart';
-import '../widgets/product_item.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
-
   // const ProductOverviewScreen({Key? key}) : super(key: key);
 
   @override
@@ -14,18 +10,8 @@ class ProductOverviewScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('DrigShop'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, index) =>
-            ProductItem(product: loadedProducts[index]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
+
